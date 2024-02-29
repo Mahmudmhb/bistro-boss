80f153be6f4f1d786c4ac0a897e368cd84eb1d2e
mahmudmhb1 <mahmud.mhb1@gmail.com>
2024-02-29 23:11:37 +0600

    admin btn added

--- src/Pages/DeshBoard/Admin/AllUsers.jsx
+++ src/Pages/DeshBoard/Admin/AllUsers.jsx
@@ -3,18 +3,61 @@ import useAxiosSecure from "../../../Hooks/useAxiosSecure/useAxiosSecure";
 import Heading from "../../../Sheard/Heading";
 import { FaTrash } from "react-icons/fa";
 import { PiUsersThreeFill } from "react-icons/pi";
+import Swal from "sweetalert2";
 
 const AllUsers = () => {
   const axiosSecure = useAxiosSecure();
 
-  const { data: users = [] } = useQuery({
+  const { refetch, data: users = [] } = useQuery({
     queryKey: ["users"],
     queryFn: async () => {
       const res = await axiosSecure.get("/users");
       return res.data;
     },
+
     //   .then((res) => console.log(res.data)),
   });
+
+  const handleAdmin = (user) => {
+    console.log(user);
+    axiosSecure.patch(`/users/admin/${user._id}`).then((res) => {
+      if (res.data.modifiedCount > 0) {
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          title: `${user.name} now is admin!`,
+          showConfirmButton: false,
+          timer: 2000,
+        });
+        refetch();
+      }
+    });
+  };
+
+  const handleDelete = (user) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You want delete this item !",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axiosSecure.delete(`/users/${user._id}`).then((res) => {
+          if (res.data.deletedCount > 0) {
+            Swal.fire({
+              title: "Deleted!",
+              text: "Your item has been deleted.",
+              icon: "success",
+            });
+            refetch();
+          }
+        });
+      }
+    });
+  };
   return (
     <div className="w-full p-10">
       {/* <h1>this is all user page</h1> */}
@@ -42,13 +85,26 @@ const AllUsers = () => {
                   <th>{index + 1}</th>
                   <td>{user.name}</td>
                   <td>{user.email}</td>
+
                   <td>
-                    <button className="btn btn-success text-white">
-                      <PiUsersThreeFill />
-                    </button>
+                    {user.role === "admin" ? (
+                      "Admin"
+                    ) : (
+                      <>
+                        <button
+                          onClick={() => handleAdmin(user)}
+                          className="btn btn-success text-white"
+                        >
+                          <PiUsersThreeFill />
+                        </button>
+                      </>
+                    )}
                   </td>
                   <td>
-                    <button className="btn btn-info text-white">
+                    <button
+                      onClick={() => handleDelete(user)}
+                      className="btn btn-info text-white"
+                    >
                       <FaTrash></FaTrash>
                     </button>
                   </td>
