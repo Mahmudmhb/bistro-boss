9740b3e9e8861af5f72e90cdc143f396cf7af5cd
mahmudmhb1 <mahmud.mhb1@gmail.com>
2024-03-01 23:43:56 +0600

    axios update

--- src/Hooks/useAxiosSecure/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure/useAxiosSecure.jsx
@@ -4,6 +4,19 @@ export const axiosSecure = axios.create({
   baseURL: "http://localhost:5000",
 });
 const useAxiosSecure = () => {
+  axiosSecure.interceptors.request.use(
+    function (config) {
+      const token = localStorage.getItem("access-token");
+      console.log("inside token", token);
+      // Do something before request is sent
+      config.headers.authorization = `Bearer ${token}`;
+      return config;
+    },
+    function (error) {
+      // Do something with request error
+      return Promise.reject(error);
+    }
+  );
   return axiosSecure;
 };
 
